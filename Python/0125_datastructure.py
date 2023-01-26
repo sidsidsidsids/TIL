f1165c4ae5c1ba43da18c1668d32eb8b733864f7
sid <sid98816@gmail.com>
2023-01-26 12:50:02 +0900

    python datastructure basic

--- Python/0125_datastructure.py
+++ Python/0125_datastructure.py
@@ -13,9 +13,41 @@ def check(target_str):
     return print(f'문자 : {l_count}개, 숫자 : {n_count}개, 기호 : {count}개')
 
 check('q!1')
+'''
 #문자 : 개, 숫자 : 개, 기호 : 개
 '''
+sample_list = [11, 22, 33, 56, 67]
 
+# 주어진 리스트의 4번째 자리(index : 3)에 있는 항목을 제거하고 변수에 할당해주세요
+
+print("original list: ", sample_list)
+elem=sample_list.pop(3)
+print("list after: ", sample_list)
+print("elem: ", elem)
+
+# sample_list의 가장 뒤에 77를 추가해보세요
+sample_list.append(77)
+
+# 할당해놓은 변수의 값을 sample_list의 2번 index에 추가해 보세요
+sample_list.insert(2,elem)
+print("add elem: ", sample_list)
+
+my_tuple = (1, 2, 3, 4, 5, 6)
+#주어진 튜플에서 4와 5를 새로운 튜플에 할당해보세요
+new_tuple = my_tuple[3:-1]
+print(new_tuple)
+'''
+
+test_list = [1, 2, 3, 7, 4, 6, 5]
+test_list.sort()
+print(test_list)
+
+
+scores = [('eng', 88), ('sci', 90), ('mth', 80)]
+# 정렬
+print(scores)
+scores.sort(key=lambda x: x[1])
+print(scores)
 # 십진수를 이진수로 나타내는 재귀 함수
 '''
 def d(n):
@@ -27,6 +59,8 @@ print(d(N))
 '''
 
 # 입력받은 리스트들을 묶는 애너그램
+'''
 words = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
 ''.join(words)
 print(words)
+'''
