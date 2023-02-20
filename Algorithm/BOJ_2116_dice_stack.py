ed30b13901c7e9080aa757b5dfb4de76085be137
sidsidsidsids <sid98816@gmail.com>
2023-02-20 17:55:04 +0900

    incom

--- Algorithm/BOJ_2116_dice_stack.py
+++ Algorithm/BOJ_2116_dice_stack.py
@@ -1,3 +1,16 @@
 dices = int(input())
-for i in range(dices):
+
+abcdef = list(map(int,input().split()))
+
+six_index = abcdef.index(6)
+if six_index == 0 or six_index == 5:
+    survive = [0,5]
+elif six_index == 1 or six_index == 3:
+    survive = [1,3]
+elif six_index == 2 or six_index == 4:
+    survive = [2,4]
+else:
     pass
+
+for i in range(dices-1):
+
