65f34d2e4b914c49851fa7706f6af09470b50bd5
sidsidsidsids <sid98816@gmail.com>
2023-04-11 17:58:23 +0900

    algo prac

---
+++ Algorithm/BOJ/BOJ_1005_ACM_craft.py
@@ -0,0 +1,67 @@
+t = int(input())
+for tc in range(t):
+    N, M = map(int,input().split())
+    time = [0] + list(map(int,input().split()))
+    adjL = [ [] for _ in range(N+1) ]
+
+    for _ in range(M):
+        s, e = map(int,input().split())
+        adjL[e].append(s)
+
+    win = int(input())
+
+    # 가능 건물 짓기
+    print()
+    result = [0] * N
+    find = 0
+    total = 0
+    s = 0
+    save = []
+
+    while True:
+        able = []
+        for i in range(1,N+1):
+            if i not in result:
+                if not adjL[i]:
+                    if i == win:
+                        total += time[i]
+                        s = 1
+                        break
+                    able.append(i)
+                    result[find] = i
+                    find += 1
+
+        if s == 1:
+            break
+
+        if save:
+            for elem in save:
+                able.append(save)
+            save = []
+
+        built = 100000
+        for work_idx in able:
+            if built > time[work_idx]:
+                built = time[work_idx]
+        finish = []
+        for work_idx in able:
+            time[work_idx] -= built
+            if time[work_idx] == 0:
+                finish.append(work_idx)
+        total += built
+
+        if len(finish) != len(able):
+            for f in finish:
+                able.remove(f)
+            for a in able:
+                save.append(a)
+
+        for i in range(1,N+1):
+            if adjL[i]:
+                for f in finish:
+                    if f in adjL[i]:
+                        adjL[i].remove(f)
+    print(total)
+
+
+
--- Algorithm/SWEA/1267_work_order.py
+++ Algorithm/SWEA/1267_work_order.py
@@ -4,6 +4,37 @@ for tc in range(1,11):
     adjL = [ [] for _ in range(V+1) ]
 
     for i in range(E):
-        adjL[arr[2*i]].append(arr[2*i+1])
+        adjL[arr[2*i+1]].append(arr[2*i])
+
+    find = 0
+    result = [0] * V
+
+    while True:
+        # 가능한 일 찾기
+        able = []
+        for i in range(1, V+1):
+            if i not in result:
+                if not adjL[i]:
+                    # for j in range(1,V+1):
+                    #     if i in adjL[j]:
+                    #         break
+                    # else:
+                    able.append(i)
+                    result[find] = i
+                    find += 1
+                    # print(result)
+
+        if find == V:
+            break
+
+        # 일하고 잠금 풀기
+        for work in able:
+            for j in range(1,V+1):
+                if work in adjL[j]:
+                    adjL[j].remove(work)
+
+    print(f'#{tc}', *result)
+
+
 
 
