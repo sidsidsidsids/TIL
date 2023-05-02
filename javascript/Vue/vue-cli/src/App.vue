9891db88ff7ae3c28ec758ba2d2965f36d8672fc
sidsidsidsids <sid98816@gmail.com>
2023-05-02 17:55:32 +0900

    vue CLI

---
+++ javascript/Vue/vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyComponent /> <!-- 3. 사용한다 -->
+      
+    <HelloWorld msg="뷰야 반갑다"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue' // 1. 등록한다(불러온다)
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent, // 2. 등록한다.
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ javascript/Vue/vue-cli/src/components/MyChild.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>This is child component</h3>
+    <h3>ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ</h3>
+    <a href="">ㅜㅜㅜㅜㅜㅜㅜㅜ</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyChild'
+}
+</script>
+
+<style>
+
+</style>
