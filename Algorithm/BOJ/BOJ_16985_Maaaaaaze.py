3c6948a9ebf6149445dda5f62955d2e76ae620b0
sidsidsidsids <sid98816@gmail.com>
2023-05-04 14:19:47 +0900

    algo prac

--- Algorithm/BOJ/BOJ_16985_Maaaaaaze.py
+++ Algorithm/BOJ/BOJ_16985_Maaaaaaze.py
@@ -1,4 +1,5 @@
 from collections import deque
+from itertools import permutations
 
 maze = [[list(map(int,input().split())) for _ in range(5)] for __ in range(5)]
 
@@ -33,44 +34,49 @@ def maze_cases(boards):
                     for board_5 in boards[4]:
                         maze_case[4] = board_5
                         maze_input = maze_case[:]
-                        totalCase[t_idx] = maze_input
+                        perms = permutations(maze_input,5)
+                        perms_list = list(perms)
+
+                        totalCase[t_idx] = perms_list
                         t_idx += 1
     return totalCase
 
+
 def BFS(mazes):
     min_val = 5*5*5
-    for miro in mazes:
-        if miro[0][0][0] == miro[4][4][4] == 1:
-            Q = deque()
-            Q.append([0,0,0])
-            V = [[[0] * 5 for _ in range(5)] for __ in range(5)]
-            V[0][0][0] = 1
-            old = 1
-            new = 0
-            cnt = 1
+    for miros in mazes:
+        for miro in miros:
+            if miro[0][0][0] == miro[4][4][4] == 1:
+                Q = deque()
+                Q.append([0,0,0])
+                V = [[[0] * 5 for _ in range(5)] for __ in range(5)]
+                V[0][0][0] = 1
+                old = 1
+                new = 0
+                cnt = 1
 
-            while Q:
-                elem = Q.popleft()
-                old -= 1
-                z,y,x = elem[0],elem[1],elem[2]
-                for nz,ny,nx in [[z+1,y,x],[z-1,y,x],[z,y+1,x],[z,y-1,x],[z,y,x+1],[z,y,x-1]]:
-                    if 0 <= nz <= 4 and 0 <= ny <= 4 and 0 <= nx <= 4:
-                        if nz == ny == nx == 4:
-                            print('goal')
-                            if min_val > cnt:
-                                min_val = cnt
-                                break
-                        if miro[nz][ny][nx] == 1 and not V[nz][ny][nx]:
-                            print('append')
-                            Q.append([nz,ny,nx])
-                            V[nz][ny][nx] = 1
-                            new += 1
-                if old == 0:
-                    old += new
-                    new = 0
-                    cnt += 1
-                    if cnt >= min_val:
-                        break
+                while Q:
+                    elem = Q.popleft()
+                    old -= 1
+                    z,y,x = elem[0],elem[1],elem[2]
+                    for nz,ny,nx in [[z+1,y,x],[z-1,y,x],[z,y+1,x],[z,y-1,x],[z,y,x+1],[z,y,x-1]]:
+                        if 0 <= nz <= 4 and 0 <= ny <= 4 and 0 <= nx <= 4:
+                            if nz == ny == nx == 4:
+                                # print('goal')
+                                if min_val > cnt:
+                                    min_val = cnt
+                                    break
+                            if miro[nz][ny][nx] == 1 and not V[nz][ny][nx]:
+                                # print('append')
+                                Q.append([nz,ny,nx])
+                                V[nz][ny][nx] = 1
+                                new += 1
+                    if old == 0:
+                        old += new
+                        new = 0
+                        cnt += 1
+                        if cnt >= min_val:
+                            break
 
     if min_val == 5*5*5:
         min_val = -1
@@ -81,7 +87,6 @@ answers = [ [] for _ in range(5) ]
 for m in range(5):
     a, b, c, d = board_cases(maze[m])
     answers[m] = [a, b, c, d]
-print(answers[4])
 answers = maze_cases(answers)
 answer = BFS(answers)
 print(answer)
